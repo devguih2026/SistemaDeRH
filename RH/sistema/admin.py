ac415e02c455f8b67bf5bfffadd37cf46fb6ecb1
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-05 14:54:46 -0300

    Registro dos modelos no admin

--- RH/sistema/admin.py
+++ RH/sistema/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# 1. Importe seus modelos
+from .models import Funcionario, Setor, Cargo, Curso, InscricaoCurso
 
-# Register your models here.
+# 2. Registre cada um deles
+admin.site.register(Funcionario)
+admin.site.register(Setor)
+admin.site.register(Cargo)
+admin.site.register(Curso)
+admin.site.register(InscricaoCurso)
