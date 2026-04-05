79b828884dffb6617d98e81f3b884536e30d5d0d
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-05 14:53:21 -0300

    URls para testes no postman

--- RH/RH/urls.py
+++ RH/RH/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from sistema.api import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+router = DefaultRouter()
+router.register(r'funcionarios', viewsets.Funcionarioviewsets, basename='funcionario')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path("token/", TokenObtainPairView.as_view()),
+    path("refresh/token/", TokenRefreshView.as_view())
 ]
