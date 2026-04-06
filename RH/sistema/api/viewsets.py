141b469d7b6dee16262a37d896156ea7bcb3a70b
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-04-06 18:36:36 -0300

    Viewsets do django filter

--- RH/sistema/api/viewsets.py
+++ RH/sistema/api/viewsets.py
@@ -1,8 +1,9 @@
 # 1. Importa o motor principal: o ModelViewSet já vem com as funções 
 # de Listar, Criar, Ver Detalhes, Editar e Deletar prontas.
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
+from django_filters.rest_framework import DjangoFilterBackend
 
 # 2. Importa o "Tradutor" (Serializer) criado na pasta API.
 from sistema.api import serializers
@@ -21,3 +22,24 @@ class Funcionarioviewsets(viewsets.ModelViewSet):
     
     # 6. Define a "Fonte": "Vá na tabela Task e pegue todos os registros".
     queryset = models.Funcionario.objects.all()
+
+    # --- CONFIGURAÇÃO DE FILTROS E BUSCAS ---
+    
+    # Define quais motores de busca a API vai usar
+    filter_backends = [
+        DjangoFilterBackend,    # Filtros exatos (ex: ?setor=1)
+        filters.SearchFilter,   # Busca por texto (ex: ?search=Guilherme)
+        filters.OrderingFilter  # Ordenação (ex: ?ordering=-salario)
+    ]
+    
+    # Campos para filtro exato (Dropdowns no Swagger)
+    filterset_fields = ['setor', 'cargo', 'matricula']
+    
+    # Campos onde o 'search' vai procurar o texto digitado
+    search_fields = ['nome', 'cpf', 'email']
+    
+    # Campos que o usuário pode usar para ordenar a lista
+    ordering_fields = ['nome', 'salario', 'data_contratacao']
+    
+    # Ordenação padrão (caso o usuário não escolha nenhuma)
+    ordering = ['nome']
